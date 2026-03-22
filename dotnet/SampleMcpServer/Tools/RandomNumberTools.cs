c4b9906ff562293df6afe2a4f21c379b50f1d51c
Fábio Naspolini <fabionaspolini@gmail.com>
2026-03-22 20:49:31 -0300

    atualizando

--- dotnet/SampleMcpServer/Tools/RandomNumberTools.cs
+++ dotnet/SampleMcpServer/Tools/RandomNumberTools.cs
@@ -10,9 +10,26 @@ internal class RandomNumberTools
     [McpServerTool]
     [Description("Generates a random number between the specified minimum and maximum values.")]
     public int GetRandomNumber(
-        [Description("Minimum value (inclusive)")] int min = 0,
-        [Description("Maximum value (exclusive)")] int max = 100)
+        [Description("Minimum value (inclusive)")]
+        int min = 0,
+        [Description("Maximum value (exclusive)")]
+        int max = 100)
     {
         return Random.Shared.Next(min, max);
     }
+
+    // criar tool para consultar cep no serviço viacep.com.br
+    // faltou os annotations
+    [McpServerTool]
+    public async Task<string> GetCepInfoAsync(string cep)
+    {
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+
 }
